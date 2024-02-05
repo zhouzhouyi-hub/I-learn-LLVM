c19ff5ca00a814d9cd6a334298672addac99e1be
Zhouyi Zhou <zhouzhouyi@gmail.com>
2024-02-06 05:37:55 +0800

    make Typeofunique_ptr.cpp involve template substitution

--- Casting/Typeofunique_ptr.cpp
+++ Casting/Typeofunique_ptr.cpp
@@ -1,13 +1,40 @@
 #include <memory>
 #include <type_traits>
-struct ZZY {
-  int i;
+struct Task
+{
+  int mId;
+  Task(int id ) :mId(id)
+  {
+  }
+  ~Task()
+  {
+
+  }
+  static bool classof(const Task *D)
+  {return true;}
 };
+
+struct Task1
+{
+  int mId;
+  Task1(int id ) :mId(id)
+  {
+
+  }
+  ~Task1()
+  {
+
+  }
+  static bool classof(const Task * D)
+  {return true;}
+};  
 int main()
 {
-  const std::unique_ptr<ZZY> ptr(new ZZY);
+  const std::unique_ptr<Task1> ptr(new Task1(3));
+  Task D(3);
   using PtrType = typename decltype(ptr)::element_type;
-  bool areTypesEqual = std::is_same<ZZY, PtrType>::value;
-  PtrType myValue = *ptr; 
+  //bool areTypesEqual = std::is_same<ZZY, PtrType>::value;
+  bool artTypesEqual = PtrType::classof(&D);
+  //PtrType myValue = *ptr; 
   return 0;
 }
