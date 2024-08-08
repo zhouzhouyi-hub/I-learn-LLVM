15aa30feb549e112e63c86796aa866fbd767ff42
Zhouyi Zhou <zhouzhouyi@gmail.com>
2024-08-09 07:17:11 +0800

    Add exception filter

---
+++ Exceptions/exceptionfilter.cpp
@@ -0,0 +1,36 @@
+/* test exception filter: gcc -std=c++03 */
+struct bar {
+  int i;
+  int j;
+  ~bar () {}
+};
+void fun(int* ptr, int x) 
+{
+    if (ptr == 0)
+        throw ptr;
+    if (x == 0)
+        throw x;
+    return;
+}
+int gl = 0;
+void fun1(int* ptr, int x) throw(int)
+{
+  struct bar baz;
+  fun(ptr, x);
+  gl = 7;
+}
+ 
+int main()
+{
+  int ret = 0;
+  try {
+    fun1(0, 0);
+  }
+  catch (int i) {
+    ret = 0x77;
+  }
+  catch (int *) {
+    ret = 0x88; 
+  }
+  return ret;
+}
